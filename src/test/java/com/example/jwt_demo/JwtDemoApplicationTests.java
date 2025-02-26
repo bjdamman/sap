57243e5f619bec3f1a0d9f22a22e6459f3d3137d
bjdamman <@rgusvlinder2>
2025-02-26 14:24:00 +0100

    test

--- src/test/java/com/example/jwt_demo/JwtDemoApplicationTests.java
+++ src/test/java/com/example/jwt_demo/JwtDemoApplicationTests.java
@@ -6,22 +6,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@Testcontainers
 class JwtDemoApplicationTests {
 
 	//@Test
@@ -29,13 +33,27 @@ class JwtDemoApplicationTests {
 	//}
 
 
-	@Autowired
-	private MockMvc mockMvc;
+    static final MySQLContainer MY_SQL_CONTAINER;
+
+    @Autowired
+    WebTestClient webTestClient;
 
 
-	@Test
-	public void givenGreetURI_whenMockMVC_thenVerifyResponse() {
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
+        MY_SQL_CONTAINER.start();
+    }
 
-	}
+    @DynamicPropertySource
+    static void configureTestProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url",() -> MY_SQL_CONTAINER.getJdbcUrl());
+        registry.add("spring.datasource.username",() -> MY_SQL_CONTAINER.getUsername());
+        registry.add("spring.datasource.password",() -> MY_SQL_CONTAINER.getPassword());
+        registry.add("spring.jpa.hibernate.ddl-auto",() -> "create");
+
+    }
+
+
+
 
 }
