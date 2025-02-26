b980ffcdbdce9044493c142e61dfdefeca218862
bjdamman <@rgusvlinder2>
2025-02-26 14:04:00 +0100

    test

--- src/test/java/com/example/jwt_demo/JwtDemoApplicationTests.java
+++ src/test/java/com/example/jwt_demo/JwtDemoApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.jwt_demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class JwtDemoApplicationTests {
 
+	//@Test
+	//void contextLoads() {
+	//}
+
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
 	@Test
-	void contextLoads() {
+	public void givenGreetURI_whenMockMVC_thenVerifyResponse() {
+
 	}
 
 }
