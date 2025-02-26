93e1a44e0e9c6bf5b57b33e0c70976d711d45c26
bjdamman <@rgusvlinder2>
2025-02-26 15:17:34 +0100

    test

--- src/test/java/com/example/jwt_demo/JwtDemoApplicationTests.java
+++ src/test/java/com/example/jwt_demo/JwtDemoApplicationTests.java
@@ -1,57 +1,103 @@
 package com.example.jwt_demo;
 
+import com.example.jwt_demo.model.Product;
+import com.example.jwt_demo.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
-@Testcontainers
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureWebTestClient
+@SpringBootTest
+@AutoConfigureMockMvc
 class JwtDemoApplicationTests {
 
 	//@Test
 	//void contextLoads() {
 	//}
-
-
-    static final MySQLContainer MY_SQL_CONTAINER;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
-    WebTestClient webTestClient;
+    private ObjectMapper objectMapper;
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Test
+    void getProduct() throws Exception {
+
+        Product product = new Product(null,"Hiaat","2");
+
+        //ArrayList<Product> products = (ArrayList<Product>) Arrays.asList(product);
+
+        when(productRepository.findById(anyLong())).thenReturn(Optional.of(product));
+        //when(listsService.findById(anyString())).thenReturn(lookupResponse);
+
+        //Book bookToCreate = new Book(null, "New Book", "New Author");
+
+        // Send POST request
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/test/product")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andReturn();
+
+        // Deserialize response
+        //Product createdProduct = objectMapper.readValue(result.getResponse().getContentAsString(), Product.class);
+
+        System.out.println("Test");
+        // Assertions
+        //assertNotNull(createdProduct.getId());
+        //assertEquals(createdProduct.getName(), createdProduct.getName());
+        //assertEquals(createdProduct.getCategory(), createdProduct.getCategory());
 
 
-    static {
-        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
-        MY_SQL_CONTAINER.start();
+        //mockMvc.perform(get("/id/1"))
+        //        .andExpect(status().isOk())
+        //        .andExpect(jsonPath("$.type", equalTo("123456")));
+
     }
 
-    @DynamicPropertySource
-    static void configureTestProperties(DynamicPropertyRegistry registry){
-        registry.add("spring.datasource.url",() -> MY_SQL_CONTAINER.getJdbcUrl());
-        registry.add("spring.datasource.username",() -> MY_SQL_CONTAINER.getUsername());
-        registry.add("spring.datasource.password",() -> MY_SQL_CONTAINER.getPassword());
-        registry.add("spring.jpa.hibernate.ddl-auto",() -> "create");
 
-    }
+
+
+
 
 
 
